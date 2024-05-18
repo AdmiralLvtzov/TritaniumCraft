f046a051ad8de84851acf2cdbdd8b90d87edc997
AdmiralLvtzov <142896318+AdmiralLvtzov@users.noreply.github.com>
2024-05-18 19:16:19 +0100

    Fix and adjust some indentations.

--- src/main/java/net/admirallvtzov/tritaniumcraft/block/ModBlocks.java
+++ src/main/java/net/admirallvtzov/tritaniumcraft/block/ModBlocks.java
@@ -24,29 +24,29 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TritaniumCraft.MODID);
 
     public static final RegistryObject<Block> BLOCK_OF_TRITANIUM = registerBlock("block_of_tritanium", () -> new Block(
-                    BlockBehaviour.Properties.of(Material.METAL)
-                            .strength(5f)
-                            .requiresCorrectToolForDrops()),
+            BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(5f)
+                    .requiresCorrectToolForDrops()),
             ModCreativeModeTab.BLOCK_TAB_OF_TRITANIUMCRAFT);
 
     public static final RegistryObject<Block> BLOCK_OF_UNITRITANIUM = registerBlock("block_of_unitritanium", () -> new Block(
-                    BlockBehaviour.Properties.of(Material.METAL)
-                            .strength(5f)
-                            .requiresCorrectToolForDrops()),
+            BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(5f)
+                    .requiresCorrectToolForDrops()),
             ModCreativeModeTab.BLOCK_TAB_OF_TRITANIUMCRAFT);
 
     public static final RegistryObject<Block> TRITANIUM_ORE = registerBlock("tritanium_ore", () -> new DropExperienceBlock(
             BlockBehaviour.Properties.of(Material.STONE)
                     .strength(3f)
                     .requiresCorrectToolForDrops(),
-                    UniformInt.of(1,2)),
+            UniformInt.of(1,2)),
             ModCreativeModeTab.BLOCK_TAB_OF_TRITANIUMCRAFT);
 
     public static final RegistryObject<Block> DEEPSLATE_TRITANIUM_ORE = registerBlock("deepslate_tritanium_ore", () -> new DropExperienceBlock(
             BlockBehaviour.Properties.of(Material.STONE)
                     .strength(4.5f)
                     .requiresCorrectToolForDrops(),
-                    UniformInt.of(1,3)),
+            UniformInt.of(1,3)),
             ModCreativeModeTab.BLOCK_TAB_OF_TRITANIUMCRAFT);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
