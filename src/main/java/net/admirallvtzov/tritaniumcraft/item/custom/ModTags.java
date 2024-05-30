53c53b98a4740a430ecd2cb896fcd89f119fd713
AdmiralLvtzov <142896318+AdmiralLvtzov@users.noreply.github.com>
2024-05-30 01:02:48 +0100

    Creates a new tier of tools.

---
+++ src/main/java/net/admirallvtzov/tritaniumcraft/item/custom/ModTags.java
@@ -0,0 +1,29 @@
+package net.admirallvtzov.tritaniumcraft.item.custom;
+
+import net.admirallvtzov.tritaniumcraft.TritaniumCraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+
+    public static class Blocks {
+
+        public static final TagKey<Block> NEEDS_TRITANIUM_TOOL = tag("needs_tritanium_tool");
+
+        private static TagKey<Block> tag(String name) {
+
+            return BlockTags.create(new ResourceLocation(TritaniumCraft.MODID, name));
+
+        }
+
+        private static TagKey<Block> forgeTag(String name) {
+
+            return BlockTags.create(new ResourceLocation("forge", name));
+
+        }
+
+    }
+
+}
---
+++ src/main/java/net/admirallvtzov/tritaniumcraft/item/custom/ModToolTiers.java
@@ -0,0 +1,36 @@
+package net.admirallvtzov.tritaniumcraft.item.custom;
+
+import net.admirallvtzov.tritaniumcraft.TritaniumCraft;
+import net.admirallvtzov.tritaniumcraft.item.ModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+import java.util.List;
+
+public class ModToolTiers {
+
+    public static Tier TRITANIUM;
+
+    static {
+
+        TRITANIUM = TierSortingRegistry.registerTier(
+                new ForgeTier(
+                        3,
+                        1404,
+                        8.0f,
+                        3.0f,
+                        9,
+                        ModTags.Blocks.NEEDS_TRITANIUM_TOOL,
+                        () -> Ingredient.of(ModItems.TRITANIUM.get())),
+                new ResourceLocation(TritaniumCraft.MODID, "tritanium"),
+                List.of(Tiers.IRON),
+                List.of(Tiers.DIAMOND)
+        );
+
+    }
+
+}
