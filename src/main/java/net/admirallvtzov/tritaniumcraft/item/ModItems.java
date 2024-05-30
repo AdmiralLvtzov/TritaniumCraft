942f12daa55c73f7128464ca82b71698c461d119
AdmiralLvtzov <142896318+AdmiralLvtzov@users.noreply.github.com>
2024-05-30 22:41:32 +0100

    Add the sword and all the tools of Tritanium.

--- src/main/java/net/admirallvtzov/tritaniumcraft/item/ModItems.java
+++ src/main/java/net/admirallvtzov/tritaniumcraft/item/ModItems.java
@@ -2,8 +2,7 @@ package net.admirallvtzov.tritaniumcraft.item;
 
 import net.admirallvtzov.tritaniumcraft.TritaniumCraft;
 import net.admirallvtzov.tritaniumcraft.item.custom.ModToolTiers;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -78,10 +77,34 @@ public class ModItems {
     public static final RegistryObject<Item> HANDLE = ITEMS.register("handle", () -> new Item(
             new Item.Properties().tab(ModCreativeModeTab.ITEM_TAB_OF_TRITANIUMCRAFT)));
 
+    public static final RegistryObject<Item> TRITANIUM_SWORD = ITEMS.register("tritanium_sword", () -> new SwordItem(
+            ModToolTiers.TRITANIUM,
+            3, // Final Damage = Damage of fists (1) + Damage Bonus of the tier + Damage Modifier of the tool
+            -2.24f, // Final Attack Speed = Speed of fists (4) + Speed Modifier of the tool
+            new Item.Properties().tab(ModCreativeModeTab.TOOL_TAB_OF_TRITANIUMCRAFT)));
+
+    public static final RegistryObject<Item> TRITANIUM_AXE = ITEMS.register("tritanium_axe", () -> new AxeItem(
+            ModToolTiers.TRITANIUM,
+            5,
+            -3f,
+            new Item.Properties().tab(ModCreativeModeTab.TOOL_TAB_OF_TRITANIUMCRAFT)));
+
     public static final RegistryObject<Item> TRITANIUM_PICKAXE = ITEMS.register("tritanium_pickaxe", () -> new PickaxeItem(
             ModToolTiers.TRITANIUM,
-            1, // Final Damage = Damage of fists (1) + Damage Bonus of the tier + Damage Modifier of the tool
-            -2.8f, // Final Attack Speed = Speed of fists (4) + Speed Modifier of the tool
+            1,
+            -2.8f,
+            new Item.Properties().tab(ModCreativeModeTab.TOOL_TAB_OF_TRITANIUMCRAFT)));
+
+    public static final RegistryObject<Item> TRITANIUM_SHOVEL = ITEMS.register("tritanium_shovel", () -> new ShovelItem(
+            ModToolTiers.TRITANIUM,
+            1.5f,
+            -3f,
+            new Item.Properties().tab(ModCreativeModeTab.TOOL_TAB_OF_TRITANIUMCRAFT)));
+
+    public static final RegistryObject<Item> TRITANIUM_HOE = ITEMS.register("tritanium_hoe", () -> new HoeItem(
+            ModToolTiers.TRITANIUM,
+            -3,
+            0f,
             new Item.Properties().tab(ModCreativeModeTab.TOOL_TAB_OF_TRITANIUMCRAFT)));
 
     public static final RegistryObject<Item> SLEDGEHAMMER = ITEMS.register("sledgehammer", () -> new Item(
